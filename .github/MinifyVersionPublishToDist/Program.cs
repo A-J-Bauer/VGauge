5dc34c42fef24b57e1c61523dbb98961048fc588
A-J-Bauer <a.j.bauer@hotmail.com>
2024-02-28 16:24:44 +0100

    ap

--- .github/MinifyVersionPublishToDist/Program.cs
+++ .github/MinifyVersionPublishToDist/Program.cs
@@ -9,52 +9,71 @@ using System.Xml.Linq;
 
 // author: A.J.Bauer
 
-
-public static class Echo
+namespace MinifyVersionPublishToDist
 {
-    public static void WriteLine(string? message)
-    {
-        Console.WriteLine(message);
-    }
-
-    public static class Debug
+    public static class Echo
     {
         public static void WriteLine(string? message)
         {
-            Echo.WriteLine($"echo \"::debug::{message}\"");
+            Console.WriteLine(message);
         }
-    }
 
-    public static class Warning
-    {
-        public static void WriteLine(string? message, [CallerLineNumber] int line = 0, [CallerMemberName] string caller = null)
+        public static class Debug
         {
-            Echo.WriteLine($"echo \"::warning file={"Program.cs"},line={line},endLine={line},title={"warning"}::{message}\"");
+            public static void WriteLine(string? message)
+            {
+                Echo.WriteLine($"echo \"::debug::{message}\"");
+            }
         }
 
+        public static class Warning
+        {
+            public static void WriteLine(string? message, [CallerLineNumber] int line = 0, [CallerMemberName] string caller = null)
+            {
+                Echo.WriteLine($"echo \"::warning file={"Program.cs"},line={line},endLine={line},title={"warning"}::{message}\"");
+            }
+
+        }
     }
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Echo.Debug.WriteLine("Houston?");
+            Echo.Warning.WriteLine("Texas?");
+        }
+
+        Environment.Exit(0);
+    }
+    //string[] cmdargs = Environment.GetCommandLineArgs();
+    //Echo.WriteLine(cmdargs[0]);
+
+    //string currentDirectory = Directory.GetCurrentDirectory();
+    //string[] files = Directory.GetFiles(currentDirectory);
+
+    //Echo.WriteLine("Files in the current directory:");
+    //foreach (string file in files)
+    //{
+    //    Debug.WriteLine(file);
+    //}
+
+    //string houston = "Houston?";
+
+    //Console.WriteLine($"messageFromEagle={houston}");
+
+
+    //var minifier = new Microsoft.Ajax.Utilities.Minifier();
+    //var minifiedString = minifier.MinifyJavaScript(unMinifiedString);
+
+
+
+
 }
 
 
-Echo.Debug.WriteLine("Houston?");
-Echo.Warning.WriteLine("Texas?");
 
-//string[] cmdargs = Environment.GetCommandLineArgs();
-//Echo.WriteLine(cmdargs[0]);
 
-//string currentDirectory = Directory.GetCurrentDirectory();
-//string[] files = Directory.GetFiles(currentDirectory);
 
-//Echo.WriteLine("Files in the current directory:");
-//foreach (string file in files)
-//{
-//    Debug.WriteLine(file);
-//}
-
-//string houston = "Houston?";
-
-//Console.WriteLine($"messageFromEagle={houston}");
-Environment.Exit(0);
-
-//var minifier = new Microsoft.Ajax.Utilities.Minifier();
-//var minifiedString = minifier.MinifyJavaScript(unMinifiedString);
