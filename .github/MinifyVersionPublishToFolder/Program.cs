baaa3f35ee2bef13a8dd7c15f4640a7961338d7d
A-J-Bauer <a.j.bauer@hotmail.com>
2024-02-29 18:13:01 +0100

    auto

--- .github/MinifyVersionPublishToFolder/Program.cs
+++ .github/MinifyVersionPublishToFolder/Program.cs
@@ -174,8 +174,6 @@ namespace MinifyVersionPublishToDist
                     {
                         jsMin.Minify(streamReader, streamWriter);
                     }
-
-                    Echo.WriteLine("{namecheck_new_version_for_dist}={value} >> $GITHUB_ENV");
                 }
                 catch (Exception ex)
                 {
