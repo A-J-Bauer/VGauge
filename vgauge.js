12e74dac4890b8a4bed942b837d80b77e1a2d578
A-J-Bauer <a.j.bauer@hotmail.com>
2024-03-01 10:24:51 +0100

    auto

--- vgauge.js
+++ vgauge.js
@@ -1,4 +1,4 @@
-// vgauge.js 1.0.11, copyright (c) 2024 A.J.Bauer, licensed under the MIT License,see LICENSE.txt for full license text.
+// vgauge.js 1.0.12, copyright (c) 2024 A.J.Bauer, licensed under the MIT License,see LICENSE.txt for full license text.
 
 class VGauge {
     static isNonEmptyString(s) {
